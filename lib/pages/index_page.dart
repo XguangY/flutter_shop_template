fdd884fa6a731f82dfb644d1c6a6219443fb14d7
ONE_IS_ALL <1095772603@qq.com>
2020-08-15 15:50:44 +0800

    '重写main.dart'

---
+++ lib/pages/index_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('flutter shop template'),
+      ),
+      body: Center(
+        child: Text('flutter shop template'),
+      ),
+    );
+  }
+}
