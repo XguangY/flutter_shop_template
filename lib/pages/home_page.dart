7610b018f6da0dd0f27485aa565f2b9089f5f1ea
ONE_IS_ALL <1095772603@qq.com>
2020-08-16 15:38:54 +0800

    '初步使用dio“

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    getHttp();
     return Scaffold(
-      body: Center(
-        child: Text('首页'),
-      ),
-    );
+        body: Center(
+      child: Text('商城首页'),
+    ));
+  }
+
+  void getHttp() async {
+    try {
+      Response response;
+      response = await Dio().get(
+        "http://www.baidu.com",
+      );
+      return print(response);
+    } catch (e) {
+      return print(e);
+    }
   }
 }
