1ef27bb218c33a609ffeecfcf931d844d86c67fa
ONE_IS_ALL <1095772603@qq.com>
2020-08-18 00:06:04 +0800

    '伪造请求头请求数据'

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
+import '../config/http_header.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,77 +8,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  //
-  TextEditingController typeController = TextEditingController();
-  String showText = '欢迎点餐';
+  String showText = '还没有请求数据';
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('点餐单'),
+          title: Text('获取去数据'),
         ),
-        // SingleChildScrollView 是一个滚动组件，可以解决手机键盘弹起导致的越界问题
         body: SingleChildScrollView(
-          child: Container(
-            height: 1000,
             child: Column(
-              children: <Widget>[
-                // 文本框组件
-                TextField(
-                  controller: typeController,
-                  decoration: InputDecoration(
-                    contentPadding: EdgeInsets.all(10.0), // 设置边距功能
-                    labelText: '点餐类型',
-                    helperText: '请输入你的点餐类型',
-                  ),
-                  autofocus: false, // 取消自动获取焦点功能
-                ),
-                RaisedButton(
-                  onPressed: _choiceAction,
-                  child: Text('选择完毕'),
-                ),
-                Text(showText,
-                    overflow: TextOverflow.ellipsis, // 设置超出容器效果
-                    maxLines: 1)
-              ],
+          children: <Widget>[
+            RaisedButton(
+              onPressed: jike,
+              child: Text('请求数据'),
             ),
-          ),
-        ),
+            Text(showText),
+          ],
+        )),
       ),
     );
   }
 
-  // 按钮点击事件处理
-  void _choiceAction() {
-    print('开始输入你选择的类型...');
-    if (typeController.text.toString() == '') {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('点餐的类型不能为空'),
-        ),
-      );
-    } else {
-      postHttp(typeController.text.toString()).then((val) {
-        setState(() {
-          showText = val['data']['name'].toString();
-        });
+  // 调用
+  void jike() {
+    getHttp().then((val) {
+      setState(() {
+        showText = val['data'].toString();
       });
-    }
+    });
   }
-}
 
-Future postHttp(String typeText) async {
-  try {
-    Response response;
-    var data = {'name': typeText};
-    response = await Dio().post(
-      'https://www.easy-mock.com/mock/5f38f8a65f7d57560f083097/shop/post_diancan',
-      queryParameters: data,
-    );
-    return response.data;
-  } catch (e) {
-    print(e);
+  // 数据请求方法
+  Future getHttp() async {
+    try {
+      Response response;
+      Dio dio = new Dio();
+      dio.options.headers = httpHeaders;
+      response =
+          await dio.get('https://time.geekbang.org/serv/v1/column/newAll');
+      print(response.data);
+      return response.data;
+    } catch (e) {
+      return print(e);
+    }
   }
 }
