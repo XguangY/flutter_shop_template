f0d5885d0846a6ee2fff8e525f0a549ac5f2370d
ONE_IS_ALL <1095772603@qq.com>
2020-08-19 00:25:46 +0800

    '轮播图'

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+// 引入轮播插件
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,28 +10,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String homePageContent = '正在获取数据';
-
   @override
-  void initState() {
-    super.initState();
-    getHomePageContent().then((val) {
-      setState(() {
-        homePageContent = val.toString();
-      });
-    });
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('百姓生活+'),
+      ),
+      body: FutureBuilder(
+        future: getHomePageContent(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var data = json.decode(snapshot.data.toString());
+            List<Map> swiperDataList =
+                (data['data']['slides'] as List).cast(); // 顶部轮播组件数
+            return Column(
+              children: <Widget>[
+                SwiperDiy(swiperDataList: swiperDataList), //页面顶部轮播组件
+              ],
+            );
+          } else {
+            return Center(
+              child: Text('加载中'),
+            );
+          }
+        },
+      ),
+    );
   }
+}
+
+// swiper 封装
+class SwiperDiy extends StatelessWidget {
+  // 接受参数
+  final List swiperDataList;
+  SwiperDiy({this.swiperDataList});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
-          appBar: AppBar(
-            title: Text('获取首页数据'),
-          ),
-          body: SingleChildScrollView(
-            child: Text(homePageContent),
-          )),
+      height: 333,
+      child: Swiper(
+        itemCount: swiperDataList.length, // 轮播item数量
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "${swiperDataList[index]['image']}",
+            fit: BoxFit.fill,
+          );
+        },
+        pagination: new SwiperPagination(),
+        autoplay: true,
+      ),
     );
   }
 }
